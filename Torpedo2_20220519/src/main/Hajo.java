e30571433e0ef8fbf8acc2ae24687531fb52c95e
P-Andris <pillerandras0714@gmail.com>
2022-05-19 15:17:15 +0200

    Hajó elhelyezve

--- Torpedo2_20220519/src/main/Hajo.java
+++ Torpedo2_20220519/src/main/Hajo.java
@@ -4,6 +4,15 @@ public class Hajo {
     private int[] pozicio = new int[3];
     
     public String talalat(int poz) {
-        return "";
+        String sz = "";
+        for(int i = 0; i < 3; i++) {
+            if(poz == pozicio[i]) sz = "Talált";
+            else sz = "Nem talált";
+        }
+        return sz;
+    }
+    
+    public Hajo(int[] pozicio) {
+        this.pozicio = pozicio;
     }
 }
--- Torpedo2_20220519/src/main/TorpedoTeszt.java
+++ Torpedo2_20220519/src/main/TorpedoTeszt.java
@@ -2,9 +2,9 @@ package main;
 
 public class TorpedoTeszt {
     public static void main(String[] args) {
-        Hajo hajo = new Hajo();
+        Hajo hajo = new Hajo([2, 3, 4]);
         String t = hajo.talalat(4);
-        assert t.equals("talált") : "Nem jó a találat ellenőrzése";
+        assert t.equals(hajo) : "Nem jó a találat ellenőrzése";
     }
     
     public String tesztLoves(int poz) {
